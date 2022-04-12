72e4ccfc420ca323241f47fbc2b9308afbdd80d8
Zurina <bigler1205@gmail.com>
2022-04-12 22:31:07 +0200

    feat: trigger webhook

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type Simple struct {
 }
 
 func SimpleFactory(host string) Simple {
-	return Simple{"Hello", "Mathias", host}
+	return Simple{"Hello", "Mathias again", host}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
