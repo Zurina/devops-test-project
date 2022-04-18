549f00b595844cf9667600199b17dd871ab80543
Zurina <bigler1205@gmail.com>
2022-04-18 15:57:47 +0200

    bug: introduce bug

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type Simple struct {
 }
 
 func SimpleFactory(host string) Simple {
-	return Simple{"Hello", "Mathias again", host}
+	return Simple{"Hello", "Mathias again", "fail"}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
